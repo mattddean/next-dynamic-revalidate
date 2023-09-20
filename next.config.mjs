e2a2dfe1bb8bf7eaaa0208fde5130599592bf9e5
Matt Dean <mdean400@gmail.com>
2023-09-20 07:47:23 -0600

    fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,9 @@ import { env } from './src/env.cjs'
 // Simple access to make sure TypeScript doesn't strip out our env import when compiling
 env.NEXT_PUBLIC_SITE_URL
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig
+export default config
